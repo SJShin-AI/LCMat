1f8b5e08faf613e5cf9c5c7b4be02e4c4e6998c3
SJShin-AI <85482956+SJShin-AI@users.noreply.github.com>
2023-02-17 15:17:51 +0900

    Update LCMat_CIFAR10.sh

--- LCMat_main/bash/LCMat_CIFAR10.sh
+++ LCMat_main/bash/LCMat_CIFAR10.sh
@@ -9,7 +9,7 @@ for seed in 0 1 2
 do
 for se in 10
 do
-CUDA_VISIBLE_DEVICES=3 python3 -u main.py --fraction ${frac} --dataset CIFAR10 --data_path ~/datasets --model ResNet18 -se ${se} -so SGD --optimizer SGD --lr 0.1 -sp ./result --batch 128 --selection Submodular --submodular ${mod} --submodular_greedy NaiveGreedy --backpack True --kernel worst --eps ${eps} --seed ${seed}
+CUDA_VISIBLE_DEVICES=3 python3 -u main.py --fraction ${frac} --dataset CIFAR10 --data_path ~/datasets --model ResNet18 -se ${se} --optimizer SGD --lr 0.1 -sp ./result --batch 128 --selection Submodular --submodular ${mod} --submodular_greedy NaiveGreedy --backpack True --kernel worst --eps ${eps} --seed ${seed}
 done
 done
 done
